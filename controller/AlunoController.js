532100f330c49824ad27d7eb96d3bef317be476d
Bruno Sato <bruno.sato@live.com>
2015-10-18 22:20:55 -0200

    organize controller

--- controller/AlunoController.js
+++ controller/AlunoController.js
@@ -3,32 +3,21 @@ var AlunoModel = require('../model/AlunoModel');
 function AlunoController() {}
 
 AlunoController.prototype.findOne = function(req, res) {
-
   var _id = req.params._id;
 
   AlunoModel.find(_id).then(function(result){
-
     res.json(result || []);
-
   }).catch(function(err) {
-
     console.log(err);
-
   });
 };
 
 AlunoController.prototype.findAll = function(req, res) {
-
   AlunoModel.findAll({}).then(function(result){
-
     res.json(result || []);
-
   }).catch(function(err) {
-
     console.log(err);
-
   });
-
 };
 
 AlunoController.prototype.create = function(req, res) {
